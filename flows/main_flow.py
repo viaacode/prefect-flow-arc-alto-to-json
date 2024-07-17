69ff3baec2fc157de117a83a171bbdf4ae61999a
Miel Vander Sande <miel.vandersande@meemoo.be>
2024-07-17 15:05:01 +0200

    Complete sparql query

--- flows/main_flow.py
+++ flows/main_flow.py
@@ -9,39 +9,61 @@ from prefect_aws import AwsCredentials
 from prefect_meemoo.triplydb.credentials import TriplyDBCredentials
 from prefect_meemoo.config.last_run import get_last_run_config, save_last_run_config
 
-# SPARQL endpoint and query
-SPARQL_ENDPOINT = "http://example.com/sparql"
-SPARQL_QUERY = """
-SELECT ?file WHERE {
-  ?s ?p ?file .
-  FILTER (regex(str(?file), ".*\\.txt$"))
-}
-"""
 
 # Task to execute SPARQL query via API call and get file list
-@task
-def get_url_list(triplydb_credentials: TriplyDBCredentials, since: datetime=None):
+@task()
+def get_url_list(
+    triplydb_credentials: TriplyDBCredentials,
+    sparql_endpoint: str,
+    since: str = None,
+):
+    logger = get_run_logger()
+
+    filter_clause = ""
+    if since is not None:
+        filter_clause = f"""
+        ?id prov:wasDerivedFrom/schema:dateModified ?modified .
+        FILTER (?modified >= {since} )
+        """
+
+    sparql_query = f"""
+    SELECT DISTINCT ?alto_url
+    WHERE {{
+        ?id a premis:File;
+            premis:storedAt/rdf:value ?alto_url ;
+            ebucore:hasMimeType "application/xml" ;
+            rel:isi/haObj:isTranscriptionCopyOf ?ie.
+              
+        {filter_clause}
+    }}
+    """
+
+    logger.info(f"Executing query on {sparql_endpoint}: {sparql_query}")
+
     response = requests.get(
-        SPARQL_ENDPOINT.format(since), params={"query": SPARQL_QUERY}, 
+        sparql_endpoint,
+        params={"query": sparql_query},
         headers={
-                "Authorization": "Bearer " + triplydb_credentials.token.get_secret_value(),
-                "Accept": "application/sparql-results+json",
-            },
+            "Authorization": "Bearer " + triplydb_credentials.token.get_secret_value(),
+            "Accept": "application/sparql-results+json",
+        },
     )
     response.raise_for_status()
     results = response.json()
-    url_list = [binding["file"]["value"] for binding in results["results"]["bindings"]]
+    url_list = [binding["alto_url"]["value"] for binding in results["results"]["bindings"]]
     return url_list
 
 
 # Task to run the Node.js script and capture stdout as JSON
-@task
+@task()
 def run_node_script(url):
     logger = get_run_logger()
     try:
         # Run the Node.js script using subprocess
         result = subprocess.run(
-            ["node", "../script/extract-text-lines-from-alto.js", url], capture_output=True, text=True
+            ["node", "../script/extract-text-lines-from-alto.js", url],
+            capture_output=True,
+            text=True,
         )
         if result.returncode != 0:
             raise Exception(f"Error running script for {url}: {result.stderr}")
@@ -52,11 +74,10 @@ def run_node_script(url):
 
 
 # Task to upload the JSON output to S3
-@task
+@task()
 def upload_to_s3(
-    content: tuple[str, str], 
-    s3_bucket_name: str, 
-    s3_credentials: AwsCredentials):
+    content: tuple[str, str], s3_bucket_name: str, s3_credentials: AwsCredentials
+):
 
     logger = get_run_logger()
 
@@ -71,12 +92,17 @@ def upload_to_s3(
     logger.info(f"Uploaded {file_name} to s3://{s3_bucket_name}/{s3_key}")
 
 
-@flow(name="prefect_flow_arc_alto_to_json", task_runner=ConcurrentTaskRunner(),  on_completion=[save_last_run_config])
+@flow(
+    name="prefect_flow_arc_alto_to_json",
+    task_runner=ConcurrentTaskRunner(),
+    on_completion=[save_last_run_config],
+)
 def main_flow(
     s3_bucket_name: str = "your-bucket-name",
     s3_block_name: str = "",
     triplydb_block_name: str = "triplydb",
-    full_sync: bool = False
+    sparql_endpoint: str = "https://api.meemoo-int.triply.cc/datasets/meemoo/knowledge-graph/services/knowledge-graph/sparql",
+    full_sync: bool = False,
 ):
     # Load credentials
     triply_creds = TriplyDBCredentials.load(triplydb_block_name)
@@ -86,9 +112,15 @@ def main_flow(
     if not full_sync:
         last_modified_date = get_last_run_config("%Y-%m-%d")
 
-    url_list = get_url_list(triply_creds, since=last_modified_date if not full_sync else None)
+    url_list = get_url_list(
+        triply_creds,
+        sparql_endpoint,
+        since=last_modified_date if not full_sync else None,
+    )
     outputs = run_node_script.map(url_list)
     upload_results = upload_to_s3.map(outputs, s3_credentials, s3_bucket_name)
+    return upload_results
+
 
 if __name__ == "__main__":
     main_flow()
